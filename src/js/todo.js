5cca1c9fdec37fdac3cd79edfe424164d81aedfc
Robin_Lontra <189180083+LontraSoft@users.noreply.github.com>
2024-12-17 12:03:56 -0600

    Add todo checklist

--- src/js/todo.js
+++ src/js/todo.js
@@ -4,3 +4,28 @@ class TodoChecklistItem {
 	this.isCompleted = isCompleted;
     }
 }
+
+class TodoChecklist {
+    #checklist;
+    
+    constructor() {
+	this.#checklist = [];
+    }
+    
+    addChecklistItem(description, isCompleted = false) {
+	this.#checklist.push(new TodoChecklistItem(description, isCompleted));
+	return this;
+    }
+
+    removeChecklistItem(indexOfRemovedItem) {
+	this.#checklist.splice(indexOfRemovedItem, 1);
+    }
+
+    resetChecklist() {
+	this.#checklist.splice(0, this.#checklist.length);
+    }
+
+    isEmpty() {
+	return this.#checklist.length === 0;
+    }
+}
