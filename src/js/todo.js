4a7f3d9076d5cff1039e4c0bad6b56d6bb921659
Robin_Lontra <189180083+LontraSoft@users.noreply.github.com>
2024-12-17 12:33:17 -0600

    Add Todo class

--- src/js/todo.js
+++ src/js/todo.js
@@ -1,3 +1,5 @@
+import Priority from './priority';
+
 class TodoChecklistItem {
     constructor(description, isCompleted = false) {
 	this.description = description;
@@ -29,3 +31,60 @@ class TodoChecklist {
 	return this.#checklist.length === 0;
     }
 }
+
+class Todo {
+    #name;
+    #description;
+    #dueDate;
+    #priority;
+    #notes;
+    #checklist;
+    
+    constructor(name,
+		description = "",
+		dueDate = new Date() + 1,
+		priority = Priority.MEDIUM,
+		notes = "")
+    {
+	this.#name = name;
+	this.#description = description;
+	this.#dueDate = dueDate;
+	this.#priority = priority;
+	this.#notes = notes;
+	this.#checklist = new TodoChecklist();
+    }
+
+    get name() { return this.#name; }
+    set name(newName) { this.#name = newName; }
+
+    get description() { return this.#description; }
+    set description(newDescription) { this.#description = newDescription; }
+
+    get dueDate() { return this.#dueDate; }
+    set dueDate(newDueDate) { this.#dueDate = newDueDate; }
+
+    get priority() { return this.#priority; }
+    set priority(newPriority) {
+	if (!newPriority instanceof Priority) {
+	    throw "Attempted to assign invalid priority"
+	}
+	this.#priority = newPriority;
+    }
+
+    get notes() { return this.#notes }
+    set notes(newNotes) { this.#notes = newNotes; }
+
+    get checklist() {
+	return this.#checklist;
+    }
+
+    hasChecklist() {
+	return !this.#checklist.isEmpty();
+    }
+
+    addChecklistItem(description, isCompleted = false) {
+	this.#checklist.addChecklistItem(description, isCompleted);
+    }
+}
+
+export default Todo;
