8722a3129b79fa97640e5c66fdab85900760434c
Robin_Lontra <189180083+LontraSoft@users.noreply.github.com>
2025-01-13 20:31:11 -0600

    Add isProjectsEmpty method

--- src/js/project_manager.js
+++ src/js/project_manager.js
@@ -4,6 +4,10 @@ import Priority from './priority';
 class ProjectManager {
     #projects;
 
+    isProjectsEmpty() {
+	return this.#projects.length === 0;
+    }
+
     constructor(projectList = []) {
 	this.#projects = projectList;
     }
