89bad42a8fdb751e14ccb3e1aa5c1149e8689330
Robin_Lontra <189180083+LontraSoft@users.noreply.github.com>
2025-01-14 00:33:47 -0600

    Add constants missing from previous commits

--- src/js/html_constants.js
+++ src/js/html_constants.js
@@ -8,6 +8,9 @@ export const PROJECT_PRIORITY_DROPDOWN = 'project-priority-dropdown';
 export const PROJECT_REMOVE_BUTTON = 'project-remove-button';
 
 export const TODO_LIST = 'todo-list';
+export const ADD_TODO_CONTAINER = 'add-todo-container';
+export const ADD_TODO_INPUT = 'add-todo-input';
+export const ADD_TODO_BUTTON = 'add-todo-button';
 export const TODO_CONTAINER = 'todo-container';
 export const TODO_SIDEBAR = 'todo-sidebar';
 export const TODO_COMPLETE_BUTTON = 'todo-complete-button';
@@ -23,6 +26,7 @@ export const ADD_CHECKLIST_ITEM_BUTTON = 'add-checklist-item-button';
 export const TODO_CHECKLIST_ITEM = 'todo-checklist-item';
 export const TODO_CHECKLIST_ITEM_DESCRIPTION = 'checklist-item-description';
 export const TODO_CHECKLIST_ITEM_CHECKBOX = 'checklist-item-checkbox';
+export const TODO_CHECKLIST_ITEM_REMOVE_BUTTON = 'checklist-item-remove-button';
 export const TODO_REMOVE_BUTTON = 'todo-remove-button';
 
 export const PRIORITY_DROPDOWN = 'priority-dropdown';
