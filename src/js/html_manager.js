a5ef7e24795ef863bce54adde450d5ea521e8aca
Robin_Lontra <189180083+LontraSoft@users.noreply.github.com>
2025-01-13 22:30:12 -0600

    Add loadProjects

--- src/js/html_manager.js
+++ src/js/html_manager.js
@@ -69,6 +69,12 @@ class htmlManager {
 	return this;   
     }
 
+    loadProjects(projectList) {
+	let thisHtmlHandler = this;
+	for (const project of projectList) {
+	    thisHtmlHandler.appendProject(project);
+	}
+    }
 }
 
 export default htmlManager;
