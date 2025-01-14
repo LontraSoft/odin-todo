47bc9a9e183c52228535144013211fcacceefa66
Robin_Lontra <189180083+LontraSoft@users.noreply.github.com>
2025-01-13 21:46:44 -0600

    Add htmlHandler data and constructor

--- src/js/html_manager.js
+++ src/js/html_manager.js
@@ -2,6 +2,18 @@ import * as HTML_CONSTANTS from './html_constants';
 import HtmlGenerator from './html_generator';
 
 class htmlHandler {
+    #win;
+    #doc;
+    #htmlGenerator;
+    #projectsContainer;
+    
+    constructor(win, doc) {
+	this.#win = win;
+	this.#doc = doc;
+	this.#htmlGenerator = new HtmlGenerator(win, doc);
+	this.#projectsContainer = this.#doc.querySelector(`#${HTML_CONSTANTS.PROJECTS_CONTAINER}`);
+    }
+    
 }
 
 export default htmlHandler;
