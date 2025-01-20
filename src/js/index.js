38ccf7b05acfc1109ae17e0cd5086e9f8bf57b71
Robin_Lontra <189180083+LontraSoft@users.noreply.github.com>
2025-01-19 23:41:28 -0600

    Add function to save initial testing environment

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,9 @@ import {Todo, TodoChecklistItem} from './todo';
 import HtmlManager from './html_manager';
 import ProjectManager from './project_manager';
 
+let projectManager = new ProjectManager();
+let htmlManager = new HtmlManager(window, document);
+
 function createExampleProject() {
     let exampleProject = new Project('Example Project');
     let exampleTodo1 = new Todo('Todo 1');
@@ -20,5 +23,26 @@ function createExampleProject() {
     exampleProject.addTodo(exampleTodo2);
 
     return exampleProject;
+}
+
+function saveTestingEnvironment() {
+    let testProjectManager = new ProjectManager();
+
+    let testProject1 = createExampleProject();
+    let testProject2 = createExampleProject();
+    let testProject3 = createExampleProject();
+    let testProject4 = createExampleProject();
+
+    testProject1.name = 'Test Project 1';
+    testProject2.name = 'Test Project 2';
+    testProject3.name = 'Test Project 3';
+    testProject4.name = 'Test Project 4';
+
+    testProjectManager.addProject(testProject4);
+    testProjectManager.addProject(testProject3);
+    testProjectManager.addProject(testProject2);
+    testProjectManager.addProject(testProject1);
+
+    testProjectManager.saveProjects();
 }
 
