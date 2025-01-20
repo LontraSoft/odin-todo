bb45d81241fc11acf51d58e5513be2266501c417
Robin_Lontra <189180083+LontraSoft@users.noreply.github.com>
2025-01-20 00:43:19 -0600

    Add ability to change project name in project manager

--- src/js/project_manager.js
+++ src/js/project_manager.js
@@ -43,6 +43,12 @@ class ProjectManager {
 	return this;
     }
 
+    updateProjectName(projectIndex, newName) {
+	this.#projects[projectIndex].name = newName;
+	this.saveProjects();
+	return this;
+    }
+
     addTodo(projectIndex, todo) {
 	if (!this.isProjectIndexValid(projectIndex)) {
 	    console.error(`Attempted to add to non-existant project: Id ${projectIndex}`);
