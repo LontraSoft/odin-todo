6d1f95064373495780eea9a3bbcb555720e5ebf7
Robin_Lontra <189180083+LontraSoft@users.noreply.github.com>
2025-01-19 22:33:24 -0600

    Add imports

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,7 @@
 import '../css/index.css'
+import Priority from './priority';
+import Project from './project';
+import {Todo, TodoChecklistItem} from './todo';
+import HtmlManager from './html_manager';
+import ProjectManager from './project_manager';
+
